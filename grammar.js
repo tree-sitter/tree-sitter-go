9ba8813be54a6518dde6e67a2dc6839264123e25
Diego Marcilio <diego.marcilio@uber.com>
2022-12-07 09:30:25 +0100

    Exposing `statement_list` in grammar

--- grammar.js
+++ grammar.js
@@ -425,11 +425,11 @@ module.exports = grammar({
 
     block: $ => seq(
       '{',
-      optional($._statement_list),
+      optional($.statement_list),
       '}'
     ),
 
-    _statement_list: $ => choice(
+    statement_list: $ => choice(
       seq(
         $._statement,
         repeat(seq(terminator, $._statement)),
@@ -591,13 +591,13 @@ module.exports = grammar({
       'case',
       field('value', $.expression_list),
       ':',
-      optional($._statement_list)
+      optional($.statement_list)
     ),
 
     default_case: $ => seq(
       'default',
       ':',
-      optional($._statement_list)
+      optional($.statement_list)
     ),
 
     type_switch_statement: $ => seq(
@@ -625,7 +625,7 @@ module.exports = grammar({
       'case',
       field('type', commaSep1($._type)),
       ':',
-      optional($._statement_list)
+      optional($.statement_list)
     ),
 
     select_statement: $ => seq(
@@ -639,7 +639,7 @@ module.exports = grammar({
       'case',
       field('communication', choice($.send_statement, $.receive_statement)),
       ':',
-      optional($._statement_list)
+      optional($.statement_list)
     ),
 
     _expression: $ => choice(
