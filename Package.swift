e27b8eb43a1213d5f2e22f5bc87ddbc7614929f1
Amaan Qureshi <amaanq12@gmail.com>
2024-09-02 04:53:06 -0400

    build: update bindings (again)

--- Package.swift
+++ Package.swift
@@ -6,41 +6,54 @@ let package = Package(
     products: [
         .library(name: "TreeSitterGo", targets: ["TreeSitterGo"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+    ],
     targets: [
-        .target(name: "TreeSitterGo",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
+        .target(
+            name: "TreeSitterGo",
+            dependencies: [],
+            path: ".",
+            exclude: [
+                "Cargo.toml",
+                "Makefile",
+                "binding.gyp",
+                "bindings/c",
+                "bindings/go",
+                "bindings/node",
+                "bindings/python",
+                "bindings/rust",
+                "prebuilds",
+                "grammar.js",
+                "package.json",
+                "package-lock.json",
+                "pyproject.toml",
+                "setup.py",
+                "test",
+                "examples",
+                ".editorconfig",
+                ".github",
+                ".gitignore",
+                ".gitattributes",
+                ".gitmodules",
+            ],
+            sources: [
+                "src/parser.c",
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterGoTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterGo",
+            ],
+            path: "bindings/swift/TreeSitterGoTests"
+        )
     ],
     cLanguageStandard: .c11
 )
