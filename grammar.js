c859d4eea71c2728995ebaaa35c205f2390d0156
Alon Livne <alon.livne@wiz.io>
2025-07-20 13:21:59 +0300

    added support for generic type aliases

--- grammar.js
+++ grammar.js
@@ -259,6 +259,7 @@ module.exports = grammar({
 
     type_alias: $ => seq(
       field('name', $._type_identifier),
+      field('type_parameters', optional($.type_parameter_list)),
       '=',
       field('type', $._type),
     ),
